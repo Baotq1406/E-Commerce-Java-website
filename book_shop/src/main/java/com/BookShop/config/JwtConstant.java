56c07539b5eac20040c07ddfaa82d2033809bfb6
Baotq1406 <quocbaotk1406@gmail.com>
2024-12-18 15:53:59 +0700

    create DB and API

---
+++ book_shop/src/main/java/com/BookShop/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.BookShop.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="dfasdgfasgbasdfasdfawefasdgasdfasdfasdfasdfawefasdvfsdv";
+	public static final String JWT_HEADER="Authorization";
+}
